df152227b5a3ae539ceab983dec0f803d6c1b240
Keith Johnson <KeithYJohnson@gmail.com>
2016-04-25 17:15:44 -0400

    adding javascript.rb

---
+++ javascript.rb
@@ -0,0 +1,12 @@
+mport Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('quotes', {path: '/'});
+});
+
+export default Router;
